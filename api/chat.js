ce5139a9674ca4e70afa4a11352cd2debf4dffd6
AL2031 <alexlin2031@icloud.com>
2026-04-05 18:46:37 -0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${process.env.API}`
+      "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
     },
     body: JSON.stringify({
       model: "llama-3.3-70b-versatile",
